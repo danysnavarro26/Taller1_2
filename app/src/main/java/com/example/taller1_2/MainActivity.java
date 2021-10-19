157605a0866139ed37299c97bbf5da837c88401b
Williams <wthomas1903@gmail.com>
2021-10-18 21:30:39 -0500

    TAL-7 Código agregado

--- app/src/main/java/com/example/taller1_2/MainActivity.java
+++ app/src/main/java/com/example/taller1_2/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.taller1_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText valorA, valorB, valorC;
+    Button btnCalc;
+    TextView tv5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        valorA = findViewById(R.id.editTextTextPersonName);
+        valorB = findViewById(R.id.editTextTextPersonName4);
+        valorC = findViewById(R.id.editTextTextPersonName5);
+        btnCalc =findViewById(R.id.calcular);
+        tv5 = findViewById(R.id.textView6);
+
+        btnCalc.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                double a = Double.parseDouble(valorA.getText().toString());
+                double b = Double.parseDouble(valorB.getText().toString());
+                double c = Double.parseDouble(valorC.getText().toString());
+                double x1 = (-b+(Math.sqrt(Math.pow(b,2)-(4*a*c))))/(2*a);
+                double x2 = (-b-(Math.sqrt(Math.pow(b,2)-(4*a*c))))/(2*a);
+                String r1 = String.format("%.2f", x1);
+                String r2 = String.format("%.2f", x2);
+                tv5.setText("X1: "+r1+"  X2: "+r2);
+
+            }
+        });
     }
 }
